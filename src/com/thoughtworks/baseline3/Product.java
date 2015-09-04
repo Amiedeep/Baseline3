8e70cb2ead92ee2cd44fed542812151697b69aca
Amandeep Singh <amandees@thoughtworks.com>
2015-09-04 18:01:31 +0530

    Adding spec for roundTax to return 4.70 as tax amount when i pass 47.73 as price of product

--- src/com/thoughtworks/baseline3/Product.java
+++ src/com/thoughtworks/baseline3/Product.java
@@ -1,6 +1,8 @@
 //this is an interface which other product items inherit.
 package com.thoughtworks.baseline3;
 
+import static java.lang.Math.round;
+
 abstract class Product {
 
     abstract double calculateTax();
@@ -8,6 +10,12 @@ abstract class Product {
     public double roundTax(double price, int taxPercentage) {
         double taxAmount = (price*taxPercentage)/100;
         taxAmount = (int)(taxAmount*100);
+        if(taxAmount%10 == 5) {
             return (double)taxAmount/100;
+        }
+        else {
+            taxAmount = (int)round((double)taxAmount / 10);
+            return (double)taxAmount/10;
+        }
     }
 }
--- test/com/thoughtworks/baseline3/ProductTest.java
+++ test/com/thoughtworks/baseline3/ProductTest.java
@@ -18,4 +18,15 @@ public class ProductTest {
         };
         assertEquals(4.75, product.roundTax(47.50, 10));
     }
+
+    @Test
+    public void shouldReturnMeTheCeilValueIfIPassDoubleWithSecondDecimalLessThanFive() {
+        product = new Product() {
+            @Override
+            double calculateTax() {
+                return 0;
+            }
+        };
+        assertEquals(4.70, product.roundTax(47.33, 10));
+    }
 }
